7598af6c927453bff5528a3035cff916e1627295
Ismail Mbarack (Nas) <isunaslabs@users.noreply.github.com>
2022-09-30 14:17:07 +0800

    before merging to main

--- packages/app-base/src/pages/Orders.tsx
+++ packages/app-base/src/pages/Orders.tsx
@@ -1,9 +1,11 @@
 import OrdersSummary from "../components/OrdersSummary";
+import OrdersTable from "../components/OrdersTable";
 
 const Orders = () => {
   return (
     <div className={"flex flex-col lg:gap-[1.875rem] gap-[0.9375rem]"}>
       <OrdersSummary />
+      <OrdersTable />
     </div>
   );
 };
