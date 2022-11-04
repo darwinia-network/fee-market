c76072708a3eaa1a2a29f4f3431012d077280a7c
JayJay1024 <lygm1062@gmail.com>
2022-11-04 10:28:00 +0800

    fix eth slot index

--- packages/app-utils/src/order.ts
+++ packages/app-utils/src/order.ts
@@ -1,6 +1,17 @@
+import { isEthApi } from "./api";
+
 export const formatOrderId = (nonce: string) => {
   if (nonce.startsWith("0x")) {
     return `${nonce.slice(0, 4)}...${nonce.slice(-4)}`;
   }
   return `#${nonce}`;
+};
+
+export const adaptSlotIndex = (api: unknown, slot: number) => {
+  // for eth "slot === 1" means "Slot #1", but for polkadot "slot === 0" means "Slot #1"
+  // here we convert to polkadot format
+  if (isEthApi(api) && slot > 0) {
+    return slot - 1;
+  }
+  return slot;
 };
