22356ca31702c6f664e3e390d5c2d569acaf4b29
JayJay1024 <lygm1062@gmail.com>
2024-03-15 17:02:03 +0800

    Warning no longer supported and maintained

---
+++ packages/app/src/App2.tsx
@@ -0,0 +1,7 @@
+export default function App2() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center px-5">
+      <span className="text-14-bold">This dapps is no longer supported and maintained!</span>
+    </div>
+  );
+}
